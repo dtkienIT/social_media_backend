1ac1158c13488e90748af2edc9a75fccb88a2f0e
Kien Do <dotrungkien1382003@gmail.com>
2026-01-21 23:27:44 +0700

    fix: import authController in routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { register, login } = require('../controllers/auth.controller');
 const uploadCloud = require('../middlewares/upload');
+const authController = require('../controllers/auth.controller');
 
 router.post('/register', uploadCloud.single('avatar'), authController.register);
 router.post('/register', register);
