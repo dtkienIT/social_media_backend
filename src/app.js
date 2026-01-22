3888599bcc0e1f9fa192f0a42480af9667eccf2a
Kien Do <dotrungkien1382003@gmail.com>
2026-01-22 09:44:05 +0700

    add new feature

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const userRoutes = require('./routes/user.routes');
 const path = require('path');
 
 const app = express();
-app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(cors());
 app.use(express.json());
 app.use('/api/auth', authRoutes);
